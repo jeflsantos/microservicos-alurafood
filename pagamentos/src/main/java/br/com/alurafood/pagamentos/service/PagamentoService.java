b924452840e15bcc30e0be1efb14f77e01cbf479
Jefferson Luis <jeflsantos@hotmail.com>
2022-11-27 00:50:50 -0300

    AluraFood - CircuitBreak e Fallback

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -73,4 +73,14 @@ public class PagamentoService {
         pedidoClient.atualizaPagamento(pagamento.get().getPedidoId());
     }
 
+    public void alteraStatus(Long id) {
+        Optional<Pagamento> pagamento = pagamentoRepository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO_SEM_INTEGRACAO);
+        pagamentoRepository.save(pagamento.get());
+    }
 }
